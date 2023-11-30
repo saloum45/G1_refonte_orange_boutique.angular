5fbb3ced7577677d344f68fba624d5013a9e7d85
salem malick mohamed <saloumfall45@gmail.com>
2023-11-30 13:51:39 +0000

    dash in progress

---
+++ src/app/admin/components/produit/produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  standalone: true,
+  imports: [],
+  templateUrl: './produit.component.html',
+  styleUrl: './produit.component.css'
+})
+export class ProduitComponent {
+
+}
